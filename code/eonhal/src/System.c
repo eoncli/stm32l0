1e1fbd2f59f619adefd19b75f5a60ee28ff36bca
FPabl0 <f.pablo1@hotmail.com>
2019-07-11 19:30:49 -0500

    System.c added

---
+++ code/eonhal/src/System.c
@@ -0,0 +1,285 @@
+/**
+ ******************************************************************************
+ * @file    System.c
+ * @author 	Pablo Fuentes
+ * @version V1.0.0
+ * @brief		System Source
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "System.h"
+
+/* Millis --------------------------------------------------------------------*/
+volatile uint32_t __ticks_millis;
+void SysTick_Handler(void)
+{
+  __ticks_millis++;
+}
+
+uint32_t millis(void)
+{
+  return __ticks_millis;
+}
+/* ---------------------------------------------------------------------------*/
+
+/* System Clock Functions ----------------------------------------------------*/
+void System_HSI_32MHz(void)
+{
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  NVIC_SetPriority(SysTick_IRQn, 0);
+
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+
+  if (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_1)
+  {
+    //error
+  }
+  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+
+  LL_RCC_HSI_Enable();
+
+  /* Wait till HSI is ready */
+  while (LL_RCC_HSI_IsReady() != 1)
+  {
+  }
+  LL_RCC_HSI_SetCalibTrimming(16);
+
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLL_MUL_4, LL_RCC_PLL_DIV_2);
+
+  LL_RCC_PLL_Enable();
+
+  /* Wait till PLL is ready */
+  while (LL_RCC_PLL_IsReady() != 1)
+  {
+  }
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+  /* Wait till System clock is ready */
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  {
+  }
+  LL_Init1msTick(32000000);
+
+  LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+
+  LL_SetSystemCoreClock(32000000);
+
+  /* SysTick_IRQn interrupt configuration */
+  NVIC_SetPriority(SysTick_IRQn, 0);
+
+  LL_SYSTICK_EnableIT(); // added to enable Systick Interrupt for millis()
+}
+
+void System_HSI_8MHz(void)
+{
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+
+  if (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0)
+  {
+    //error
+  }
+  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+
+  LL_RCC_HSI_Enable();
+
+  /* Wait till HSI is ready */
+  while (LL_RCC_HSI_IsReady() != 1)
+  {
+  }
+  LL_RCC_HSI_SetCalibTrimming(16);
+
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLL_MUL_3, LL_RCC_PLL_DIV_3);
+
+  LL_RCC_PLL_Enable();
+
+  /* Wait till PLL is ready */
+  while (LL_RCC_PLL_IsReady() != 1)
+  {
+  }
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_2);
+
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+  /* Wait till System clock is ready */
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  {
+  }
+  LL_Init1msTick(8000000);
+
+  LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+
+  LL_SetSystemCoreClock(8000000);
+
+  /* SysTick_IRQn interrupt configuration */
+  NVIC_SetPriority(SysTick_IRQn, 0);
+
+  LL_SYSTICK_EnableIT(); // added to enable Systick Interrupt for millis()
+}
+
+void System_HSI_6MHz(void)
+{
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+
+  if (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0)
+  {
+    //error
+  }
+  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE2);
+
+  LL_RCC_HSI_Enable();
+
+  /* Wait till HSI is ready */
+  while (LL_RCC_HSI_IsReady() != 1)
+  {
+  }
+  LL_RCC_HSI_SetCalibTrimming(16);
+
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLL_MUL_3, LL_RCC_PLL_DIV_2);
+
+  LL_RCC_PLL_Enable();
+
+  /* Wait till PLL is ready */
+  while (LL_RCC_PLL_IsReady() != 1)
+  {
+  }
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_4);
+
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+  /* Wait till System clock is ready */
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  {
+  }
+  LL_Init1msTick(6000000);
+
+  LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+
+  LL_SetSystemCoreClock(6000000);
+
+  LL_RCC_SetI2CClockSource(LL_RCC_I2C1_CLKSOURCE_PCLK1);
+
+  /* SysTick_IRQn interrupt configuration */
+  NVIC_SetPriority(SysTick_IRQn, 0);
+
+  LL_SYSTICK_EnableIT(); // added to enable Systick Interrupt for millis()
+}
+
+void System_HSI_4MHz(void)
+{
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+
+  if (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0)
+  {
+    //error
+  }
+  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+
+  LL_RCC_HSI_Enable();
+
+  /* Wait till HSI is ready */
+  while (LL_RCC_HSI_IsReady() != 1)
+  {
+  }
+  LL_RCC_HSI_SetCalibTrimming(16);
+
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLL_MUL_3, LL_RCC_PLL_DIV_3);
+
+  LL_RCC_PLL_Enable();
+
+  /* Wait till PLL is ready */
+  while (LL_RCC_PLL_IsReady() != 1)
+  {
+  }
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_4);
+
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+  /* Wait till System clock is ready */
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  {
+  }
+  LL_Init1msTick(4000000);
+
+  LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+
+  LL_SetSystemCoreClock(4000000);
+
+  /* SysTick_IRQn interrupt configuration */
+  NVIC_SetPriority(SysTick_IRQn, 0);
+
+  LL_SYSTICK_EnableIT(); // added to enable Systick Interrupt for millis()
+}
+
+void System_MSI_2MHz(void)
+{
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  NVIC_SetPriority(SysTick_IRQn, 0);
+
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+
+  if (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0)
+  {
+    //error
+  }
+  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE3);
+
+  LL_RCC_MSI_Enable();
+
+  /* Wait till MSI is ready */
+  while (LL_RCC_MSI_IsReady() != 1)
+  {
+  }
+  LL_RCC_MSI_SetRange(LL_RCC_MSIRANGE_5);
+
+  LL_RCC_MSI_SetCalibTrimming(0);
+
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_MSI);
+
+  /* Wait till System clock is ready */
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_MSI)
+  {
+  }
+  LL_Init1msTick(2097000);
+
+  LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+
+  LL_SetSystemCoreClock(2097000);
+
+  /* SysTick_IRQn interrupt configuration */
+  NVIC_SetPriority(SysTick_IRQn, 0);
+
+  LL_SYSTICK_EnableIT(); // added to enable Systick Interrupt for millis()
+}
